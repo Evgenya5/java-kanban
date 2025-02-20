0f939f203c9053763f9bf2fdefe3f463fda0ffd8
Evgeniya <eva.sharapova1@mail.ru>
2025-02-21 01:25:49 +0300

    V1_3

--- src/api/DurationAdapter.java
+++ src/api/DurationAdapter.java
@@ -1,11 +1,11 @@
 package api;
 
 import com.google.gson.TypeAdapter;
-
 import java.time.Duration;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 import java.io.IOException;
+
 public class DurationAdapter extends TypeAdapter<Duration> {
 
     @Override
